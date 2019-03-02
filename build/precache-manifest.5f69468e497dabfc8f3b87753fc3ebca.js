7fec95725dd15b3d488e90a572e58f66c50923d1
Arcel MAENDELEO <arctirux@gmail.com>
2019-03-02 20:09:03 +0300

    03-03-1019

--- build/precache-manifest.5f69468e497dabfc8f3b87753fc3ebca.js
+++ build/precache-manifest.5f69468e497dabfc8f3b87753fc3ebca.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = [
   {
-    "revision": "c6958ec6387939cbfbde",
+    "revision": "8a8c6662cc5cd1a02669",
     "url": "/static/css/main.5798c0b8.chunk.css"
   },
   {
-    "revision": "c6958ec6387939cbfbde",
-    "url": "/static/js/main.c6958ec6.chunk.js"
+    "revision": "8a8c6662cc5cd1a02669",
+    "url": "/static/js/main.8a8c6662.chunk.js"
   },
   {
     "revision": "fdfcfda2d9b1bf31db52",
@@ -16,7 +16,7 @@ self.__precacheManifest = [
     "url": "/static/js/2.e6b9a71e.chunk.js"
   },
   {
-    "revision": "de1e957f9c672d64bd908b0d6684b656",
+    "revision": "d22ec1aec0f56d0e84cb29305bff7f37",
     "url": "/index.html"
   }
 ];
