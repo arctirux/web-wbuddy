3d4a3996a019e8e129956455ddb9841956ec1e21
Arcel MAENDELEO <arctirux@gmail.com>
2019-03-12 15:18:22 +0300

    03-03-1019

---
+++ build/precache-manifest.ac8366d2867cbaf7f47de428e86cb1c5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d45f1894e01c5f8837be",
+    "url": "/web-wbuddy/static/css/main.5798c0b8.chunk.css"
+  },
+  {
+    "revision": "d45f1894e01c5f8837be",
+    "url": "/web-wbuddy/static/js/main.d45f1894.chunk.js"
+  },
+  {
+    "revision": "b27c2a4aea7a3ae4cd6c",
+    "url": "/web-wbuddy/static/js/runtime~main.b27c2a4a.js"
+  },
+  {
+    "revision": "1a91c2258ea5e7bdc23c",
+    "url": "/web-wbuddy/static/js/2.1a91c225.chunk.js"
+  },
+  {
+    "revision": "fbb9b4d3bf593a1a7d2310a1dd9430ca",
+    "url": "/web-wbuddy/index.html"
+  }
+];
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/web-wbuddy/precache-manifest.787ae0348ceca0968a85e552e169381d.js"
+  "/web-wbuddy/precache-manifest.ac8366d2867cbaf7f47de428e86cb1c5.js"
 );
 
 workbox.clientsClaim();
