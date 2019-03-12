a5f3372673a99b26d4215a4dc2ef29b09efdcb18
Arcel MAENDELEO <arctirux@gmail.com>
2019-03-12 12:22:51 +0300

    Updates

---
+++ precache-manifest.e272d1e9d9c1042eb9fe534672e6cb15.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9307fda580d96f7dcacc",
+    "url": "/web-wbuddy/static/css/main.5798c0b8.chunk.css"
+  },
+  {
+    "revision": "9307fda580d96f7dcacc",
+    "url": "/web-wbuddy/static/js/main.9307fda5.chunk.js"
+  },
+  {
+    "revision": "3c94e21dccf78d65c3af",
+    "url": "/web-wbuddy/static/js/runtime~main.3c94e21d.js"
+  },
+  {
+    "revision": "43fed9aa41f95576edf4",
+    "url": "/web-wbuddy/static/js/2.43fed9aa.chunk.js"
+  },
+  {
+    "revision": "76a8c99c4872ed74bbe523fc2310ee13",
+    "url": "/web-wbuddy/index.html"
+  }
+];
