c20c34321af9ef554626f960bf939b79b28afcbb
Arcel MAENDELEO <arctirux@gmail.com>
2019-03-12 14:28:20 +0300

    03-03-1019

---
+++ build/precache-manifest.5a046517b91ed0aa1b1eaec30ab35786.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "039b839c55d97cee5c12",
+    "url": "/static/css/main.5798c0b8.chunk.css"
+  },
+  {
+    "revision": "039b839c55d97cee5c12",
+    "url": "/static/js/main.039b839c.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.42ac5946.js"
+  },
+  {
+    "revision": "501f5dc650eb0c469053",
+    "url": "/static/js/2.501f5dc6.chunk.js"
+  },
+  {
+    "revision": "a5414531cdfb5d0d205c4913422d00d1",
+    "url": "/index.html"
+  }
+];
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/web-wbuddy/precache-manifest.14d630109094a14c9f416e3188eba362.js"
+  "/precache-manifest.5a046517b91ed0aa1b1eaec30ab35786.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/web-wbuddy/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
